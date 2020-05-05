db4d68e0e59874967ad68ab9481f3d8cb47d1888
Cheewaca <42944307+cheewaca@users.noreply.github.com>
2020-05-05 13:45:45 +0200

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,10 +14,10 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_h870.mk
+    $(LOCAL_DIR)/ancient_h870.mk
 
 	
 COMMON_LUNCH_CHOICES := \
-    lineage_h870-user \
-    lineage_h870-userdebug \
-    lineage_h870-eng
+    ancient_h870-user \
+    ancient_h870-userdebug \
+    ancient_h870-eng
