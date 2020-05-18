5e5ab5ec1aae95c7321732c40707714db0a58f53
Michael Bardoň <wolfovelp@gmail.com>
2020-05-18 18:16:55 +0200

    WIP Kang tools from leeco

--- extract-files.sh
+++ extract-files.sh
@@ -1,12 +1,14 @@
+ 
 #!/bin/bash
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,6 +28,8 @@ set -e
 export DEVICE=h870
 export DEVICE_COMMON=msm8996-common
 export VENDOR=lge
+
+export DEVICE_BRINGUP_YEAR=2016
 
 # Use common extractor
 source "./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
